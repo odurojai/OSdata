d57415e21744e10b2018c29b158722f0de1618a1
odurojai <odurojaiye@gmail.com>
2023-04-07 21:27:38 +0100

    Added extra dash

--- ADB Essentials _ Delta Lake v2.py
+++ ADB Essentials _ Delta Lake v2.py
@@ -5,7 +5,7 @@
 # MAGIC ### Source Data for this notebook
 # MAGIC The data used is a modified version of the public data from [Lending Club](https://www.kaggle.com/wendykan/lending-club-loan-data). It includes all funded loans from 2012 to 2017. Each loan includes applicant information provided by the applicant as well as the current loan status (Current, Late, Fully Paid, etc.) and latest payment information. For a full view of the data please view the data dictionary available [here](https://resources.lendingclub.com/LCDataDictionary.xlsx).
 
-# COMMAND ----------
+# COMMAND --------------
 
 db = "delta_adb_essentials"
 
